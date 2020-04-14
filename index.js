8df50951bcb30b0f342b0e8ede3ae13f04abd7aa
Renato Flores <44614747+Renato776@users.noreply.github.com>
2020-04-14 16:14:56 -0700

    Finished first version of bisection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Printing = {
     printLog:function(text){
         console.log(text);
+    },
+    errorLog:function (error_) {
+        console.log("FATAL ERROR: "+error_);
     }
 };
 const Approx = {
@@ -16,7 +19,7 @@ const Approx = {
     bisect:function(x0,gap){
         let MAX_INTENTS = 3000;
         let FATAL_FAILURE = 0;
-        const exactitud = 0.00000000000000001;
+        const exactitud = Math.pow(10,-6);
         let a = x0;
         let y0 = this.equation(x0);
         while(this.invalid(y0)&&(FATAL_FAILURE!=MAX_INTENTS)){
@@ -28,29 +31,64 @@ const Approx = {
             Printing.printLog(this.express_interval(a,x0));
             return NaN;
         }
-        let b = cuota/inicial - a;
-        let error = 100;
-        let f_a = equation(inicial,cuota,a,n);
-        let f_b = equation(inicial,cuota,b,n);
+        if(y0==0)return a;
+        let b;
+        let y1;
+        FATAL_FAILURE = 0;
+        MAX_INTENTS = 10000;
+        b = a - gap;  //We'll start looking for any root at the left of a.
+        y1 = this.equation(b);
+        while(!this.sign_change(y0,y1)&&FATAL_FAILURE!=MAX_INTENTS){
+            b -= gap;
+            y1 = this.equation(b);
+            FATAL_FAILURE++;
+        }
+        if(FATAL_FAILURE==MAX_INTENTS){ //NO root found at the left of a. Let's search at the right.
+            FATAL_FAILURE = 0;
+            MAX_INTENTS = 10000;
+            b = a + gap;
+            y1 = this.equation(b);
+            while(!this.sign_change(y0,y1)&&FATAL_FAILURE!=MAX_INTENTS){
+                b += gap;
+                y1 = this.equation(b);
+                FATAL_FAILURE++;
+            }
+        }
+        if(FATAL_FAILURE==MAX_INTENTS){
+            Printing.errorLog("NO root found anywhere within "+this.express_interval(a - gap*MAX_INTENTS,a+gap*MAX_INTENTS))
+            return NaN;
+        }
+        FATAL_FAILURE = 0;
+        MAX_INTENTS = 50000;
+        let f_a = this.equation(a);
+        let f_b = this.equation(b);
         let x = (a+b)/2; //initial guess.
-        let f_x = equation(inicial,cuota,x,n);
-        Printing.printLog('f_a: '+f_a+"\nf_b: "+f_b);
-        while (!(f_x==0 || error<exactitud )){
-            if(sign_change(f_x,f_a)){
+        let f_x = this.equation(x);
+        let error  = 1;
+        while ((!(f_x==0 || error<exactitud ))&&FATAL_FAILURE!=MAX_INTENTS){
+            if(this.sign_change(f_x,f_a)){
                 b = x;
             }else{
                 a = x;
             }
             x = (a+b)/2;
-            f_x = equation(inicial,cuota,x,n);
-            f_b = equation(inicial,cuota,b,n);
-            f_a = equation(inicial,cuota,a,n);
+            f_x = this.equation(x);
+            f_b = this.equation(b);
+            f_a = this.equation(a);
             error = Math.abs(f_b - f_a);
+            FATAL_FAILURE++;
+        }
+        if(FATAL_FAILURE==MAX_INTENTS){
+            Printing.errorLog("Couldn't find exact enough root for an exactness of: "+exactitud+" With a total of " +
+                MAX_INTENTS+" tries.");
+            return NaN;
         }
         Printing.printLog('error: '+error);
         Printing.printLog('result: '+x);
+        return x;
     },
     sign_change : function(a,b){
+        if(this.invalid(a)||this.invalid(b))return false;
         if(a<0 && b <0) return false;
         if(a>0 && b>0) return false;
         return true;
